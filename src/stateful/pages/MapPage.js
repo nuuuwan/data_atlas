76ec162930504a897c626d43219d9dcae38b457b
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-10 20:41:28 +0530

    Added MapPage stub

---
+++ src/stateful/pages/MapPage.js
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+import './MapPage.css';
+
+export default class MapPage extends Component {
+
+  render() {
+    return (
+      <div className="div-map-page">
+        Map Page
+      </div>
+    );
+  }
+
+}
